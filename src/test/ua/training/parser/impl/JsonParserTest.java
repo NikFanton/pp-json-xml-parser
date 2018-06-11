2248c7401eff752c87d61be629eae296b5cf85f8
NikFanton <guchenko.n@gmail.com>
2018-06-11 13:34:28 +0300

    added tests for getData() method

--- src/test/ua/training/parser/impl/JsonParserTest.java
+++ src/test/ua/training/parser/impl/JsonParserTest.java
@@ -3,13 +3,19 @@ package ua.training.parser.impl;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import ua.training.entity.Currency;
+
+import java.io.File;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
 public class JsonParserTest {
-
+    public static final String PATH = "src\\resources\\file.json";
+    private JsonParser parser;
     @Before
     public void setUp() throws Exception {
+        parser = new JsonParser(new File(PATH));
     }
 
     @After
@@ -18,6 +24,8 @@ public class JsonParserTest {
 
     @Test
     public void getData() {
+        List<Currency> currencies = parser.getData();
+        assertNotNull(currencies);
     }
 
     @Test
--- src/test/ua/training/parser/impl/XmlParserTest.java
+++ src/test/ua/training/parser/impl/XmlParserTest.java
@@ -1,5 +1,35 @@
-import static org.junit.Assert.*;
+package ua.training.parser.impl;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import ua.training.entity.Currency;
+import ua.training.entity.Person;
+
+import java.io.File;
+import java.util.List;
+
+import static org.junit.Assert.assertNotNull;
 
 public class XmlParserTest {
+    private static final String PATH = "src\\resources\\file.xml";
+    private XmlParser parser;
+    @Before
+    public void setUp() throws Exception {
+        parser = new XmlParser(new File(PATH));
+    }
 
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getData() {
+        List<Person> people = parser.getData();
+        assertNotNull(people);
+    }
+
+    @Test
+    public void saveData() {
+    }
 }
